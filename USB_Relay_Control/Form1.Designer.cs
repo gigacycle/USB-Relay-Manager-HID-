e62868bf2dab501e80bf360f7b84d73aacd830e8
gigacycle1 <gigacycle@gmail.com>
2018-02-27 02:20:47 +0330

    Hello GitHub

---
+++ USB_Relay_Control/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace USB_Relay_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(14, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Devices :";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(14, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Relay 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(14, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Relay 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(14, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Relay 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(15, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Relay 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(15, 242);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Relay 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(15, 271);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(169, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Relay 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(15, 300);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(169, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Relay 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(15, 329);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(169, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "Relay 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 364);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "USB Relay";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+
+    }
+}
+
---
+++ USB_Relay_Control/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USB_Relay_Control
+{
+
+    public partial class Form1 : Form
+    {
+        int _deviceHandle = 0;
+        List<Button> _buttonList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            refreshList();
+
+            _buttonList = new List<Button>{
+                button1, button2, button3, button4,
+                button5, button6, button7, button8
+            };
+
+            if (listBox1.Items.Count > 0)
+                listBox1.SelectedIndex = 0;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_deviceHandle > 0)
+            {
+                RelayDeviceWrapper.usb_relay_device_close(_deviceHandle);
+                _deviceHandle = 0;
+            }
+
+            for (int i = 0; i < _buttonList.Count; i++){
+                _buttonList[i].BackColor = BackColor;
+                _buttonList[i].Enabled = false;
+            }
+
+            var lb = (ListBox)sender;
+            usb_relay_device_info device = (usb_relay_device_info)lb.SelectedItem;
+            _deviceHandle = RelayDeviceWrapper.usb_relay_device_open(ref device);
+            int numberOfRelays = (int)device.type;
+            
+            uint status = 0;
+            RelayDeviceWrapper.usb_relay_device_get_status(_deviceHandle, ref status);
+
+            for (int i = 0; i < numberOfRelays; i++)
+            {
+                _buttonList[i].Enabled = true;
+                if (status > numberOfRelays)
+                    _buttonList[i].BackColor = Color.Red;
+                else if (i + 1 == status)
+                    _buttonList[i].BackColor = Color.Red;
+            }
+        }
+
+        private void refreshList()
+        {
+            listBox1.Items.Clear();
+            if (RelayDeviceWrapper.usb_relay_init() != 0)
+            {
+                Console.WriteLine("Couldn't initialize!");
+                MessageBox.Show("Couldn't initialize!");
+                return;
+            }
+            else { Console.WriteLine("Initialized successfully!"); }
+
+            List<usb_relay_device_info> devicesInfos = new List<usb_relay_device_info>();
+            usb_relay_device_info deviceInfo = RelayDeviceWrapper.usb_relay_device_enumerate();
+            devicesInfos.Add(deviceInfo);
+
+            while (deviceInfo.next.ToInt32() > 0)
+            {
+                deviceInfo = (usb_relay_device_info)Marshal.PtrToStructure(deviceInfo.next, typeof(usb_relay_device_info));
+                devicesInfos.Add(deviceInfo);
+            }
+
+            foreach (var device in devicesInfos)
+            {
+                listBox1.Items.Add(device);
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (_deviceHandle <= 0)
+                return;
+
+            Button btn = (Button)sender;
+            string strNum = btn.Text.Substring(btn.Text.Length - 1, 1);
+            int num = int.Parse(strNum);
+
+            if (btn.BackColor == BackColor || btn.BackColor == Color.Transparent)
+            {
+                int openResult = RelayDeviceWrapper.usb_relay_device_open_one_relay_channel(_deviceHandle, num);
+            }
+            else
+            {
+                int closeResult = RelayDeviceWrapper.usb_relay_device_close_one_relay_channel(_deviceHandle, num);
+            }
+            btn.BackColor = (btn.BackColor == Color.Red) ? BackColor : Color.Red;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_deviceHandle > 0)
+                RelayDeviceWrapper.usb_relay_device_close(_deviceHandle);
+        }
+
+    }
+}
